6b835dedd353d4421e8bca1ca931b82f7331e515
BinuriGunawardena <gunawardenabinuri@gmail.com>
2025-05-22 08:26:02 +0530

    Use environment variable for Firebase

--- database.py
+++ database.py
@@ -1,9 +1,23 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+# cred = credentials.Certificate("serviceaccountkey.json")
+# firebase_admin.initialize_app(cred)
+# db = firestore.client()
+
+service_account_json = os.environ.get("GOOGLE_SERVICE_ACCOUNT")
+
+if service_account_json:
+    cred_dict = json.loads(service_account_json)
+    cred = credentials.Certificate(cred_dict)
+    firebase_admin.initialize_app(cred)
+else:
+    raise Exception("GOOGLE_SERVICE_ACCOUNT environment variable is missing.")
+
+
 # Initialize Firebase
-cred = credentials.Certificate("serviceaccountkey.json")
-firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 def store_prediction(user_id, collection_name, prediction_type, input_data, emotion):
