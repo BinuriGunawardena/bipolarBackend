b3f0889a62597a58614179c6030d4bc2c67ed965
BinuriGunawardena <gunawardenabinuri@gmail.com>
2025-05-25 12:23:11 +0530

    Trigger API

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask, request
 import json
-from routes import predict_text, predict_audio, predict_bipolar_stage
+from apscheduler.schedulers.background import BackgroundScheduler
+from routes import predict_text, predict_audio, predict_bipolar_stage, scheduled_bipolar_prediction
 
 app = Flask(__name__)
 
@@ -20,6 +21,11 @@ def predict_bipolar_stage_emotion():
 @app.route('/')
 def hello_world():
     return 'BIPOLAR DISORDER PREDICTION SYSTEM'
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(scheduled_bipolar_prediction, 'interval', minutes=1)
+scheduler.start()
+print("Scheduler started")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
